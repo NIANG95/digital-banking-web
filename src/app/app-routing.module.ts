73624371b67b460d5106a6292760872b2f4f01a1
Mame Sandeck Niang <niangmamesandeck@gmail.com>
2023-07-01 16:07:38 +0000

    digital-banking-web

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountsComponent } from './accounts/accounts.component';
+import { CustomerAccountsComponent } from './customer-accounts/customer-accounts.component';
+import { CustomersComponent } from './customers/customers.component';
+import { NewCustomerComponent } from './new-customer/new-customer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : "customers", component : CustomersComponent },
+  { path : "", component : CustomersComponent },
+  // { path: "", redirectTo: "/customers", pathMatch: "full" },
+  { path : "accounts", component : AccountsComponent },
+  { path : "new-customer", component : NewCustomerComponent },
+  { path : "customer-accounts/:id", component : CustomerAccountsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
